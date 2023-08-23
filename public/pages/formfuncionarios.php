6a1aa2b61959456e97500e55fb117f8ffed0cac0
lucasbaldan <lucasbaldan2014@gmail.com>
2023-08-23 15:48:25 -0300

    Update formfuncionarios.php

--- public/pages/formfuncionarios.php
+++ public/pages/formfuncionarios.php
@@ -6,10 +6,6 @@ include("./footer_menu.php");
 ?>
 
 <style>
-  .ui.celled.striped.table th {
-    text-align: center;
-  }
-
   .ui.slider.checkbox {
     padding-right: 50px;
   }
@@ -29,135 +25,131 @@ include("./footer_menu.php");
 
     <div class="ui form">
       <!-- <form id="CAD-funcionario"> -->
-        <div class="ui bottom attached tab segment active" data-tab="funcionario-geral">
-          <h3>Cadastros Gerais do Funcionário</h3>
-          <div class="ui fluid label">
-            Nome do Funcionário⠀⠀⠀
-            <div class="ui fluid icon input">
-              <input type="text" style="border-color: red;">
-              <i class="icon keyboard outline"></i>
-            </div>
+      <div class="ui bottom attached tab segment active" data-tab="funcionario-geral">
+        <h3>Cadastros Gerais do Funcionário</h3>
+        <div class="ui fluid label">
+          Nome do Funcionário⠀⠀⠀
+          <div class="ui fluid icon input">
+            <input type="text" style="border-color: red;">
+            <i class="icon keyboard outline"></i>
           </div>
-          <br><br>
-          <div class="ui fluid label">
-            Setor⠀⠀⠀
-            <div class="ui fluid input">
-              <select id="select-setor" name="selectSetor" class="select2" style="border-color: red;" required></select>
-            </div>
-          </div>
-          <!-- </form> -->
         </div>
-        <div class="ui bottom attached tab segment" data-tab="funcionario-funcionais">
-          <h3>Cadastros Funcionais do Funcionário</h3>
-          <div class="ui label">
-            Matrícula⠀⠀
-            <div class="ui input">
-              <input type="text" id="matricula" style="border-color: red;">
-            </div>
+        <br><br>
+        <div class="ui fluid label">
+          Setor⠀⠀⠀
+          <div class="ui fluid input">
+            <select id="select-setor" name="selectSetor" class="select2" style="border-color: red;" required></select>
           </div>
-          <div class="ui label">
-            Data de início⠀⠀
-            <div class="ui input">
-              <input type="date" id="dataInicio" style="border-color: red;">
-            </div>
-          </div>
-          <div class="ui label">
-            Data de Término⠀⠀
-            <div class="ui input">
-              <input type="date" id="dataTermino">
-            </div>
-          </div>
-
-          <div class="ui label">
-            Faz horário de almoço?⠀⠀
-            <div class="ui input">
-              <select id="select-almoco" name="almoco" class="select2" style="border-color: red;" required>
-                <option value="S">Sim</option>
-                <option value="N">Não</option>
-              </select>
-            </div>
-          </div>
-
-          <br><br>
-
-
-          <div class="ui label">
-            Funcão⠀⠀
-            <div class="ui input">
-              <select id="select-funcao" name="funcao-funcionario" class="select2" style="border-color: red;" required>
-              </select>
-            </div>
-          </div>
-
-          <br><br>
-
-          <div class="ui large label">
-            Dias de Trabalho - Semanal</div>
-          <br><br>
-          <div class="ui slider checkbox">
-            <input type="checkbox" name="SEG" id="SEG">
-            <label for="SEG">Segunda</label>
-          </div>
-
-          <div class="ui slider checkbox">
-            <input type="checkbox" name="TER" id="TER">
-            <label for="TER">Terça</label>
-          </div>
-
-          <div class="ui slider checkbox">
-            <input type="checkbox" name="QUA" id="QUA">
-            <label for="QUA">Quarta</label>
-          </div>
-
-          <div class="ui slider checkbox">
-            <input type="checkbox" name="QUI" id="QUI">
-            <label for="QUI">Quinta</label>
-          </div>
-
-          <div class="ui slider checkbox">
-            <input type="checkbox" name="SEX" id="SEX">
-            <label for="SEX">Sexta</label>
-          </div>
-
-          <br><br>
-
-          <div class="ui fluid label">
-            Descrição do horário de Trabalho⠀⠀⠀
-            <div class="ui fluid icon input">
-              <input type="text" id="descricaoHorario" style="border-color: red;">
-              <i class="icon keyboard outline"></i>
-            </div>
-          </div>
-
-          <br><br>
-          <button class="ui right labeled icon green button" id="addfuncional">
-            <i class="plus square outline icon"></i>
-            Adicionar / Alterar
-          </button>
-
-          <table class="ui celled striped table">
-            <thead>
-              <tr header="Registros atuais">
-                <th colspan="7" style="background-color: black; color: white;">
-                  Registros atuais
-                </th>
-              </tr>
-              <tr>
-                <th style="background-color: gray; color: white;">Matrícula</th>
-                <th style="background-color: gray; color: white;">Data Início</th>
-                <th style="background-color: gray; color: white;">Data término</th>
-                <th style="background-color: gray; color: white;">Almoço?</th>
-                <th style="background-color: gray; color: white;">Função</th>
-                <th style="background-color: gray; color: white;">Dias Trabalho</th>
-                <th style="background-color: gray; color: white;">Descrição horário</th>
-              </tr>
-            </thead>
-            <tbody>
-              <th>Nenhum Registro Cadastrado</th>
-            </tbody>
-          </table>
-
         </div>
+        <!-- </form> -->
+      </div>
+      <div class="ui bottom attached tab segment" data-tab="funcionario-funcionais">
+        <h3>Cadastros Funcionais do Funcionário</h3>
+        <div class="ui label">
+          Matrícula⠀⠀
+          <div class="ui input">
+            <input type="text" id="matricula" style="border-color: red;">
+          </div>
+        </div>
+        <div class="ui label">
+          Data de início⠀⠀
+          <div class="ui input">
+            <input type="date" id="dataInicio" style="border-color: red;">
+          </div>
+        </div>
+        <div class="ui label">
+          Data de Término⠀⠀
+          <div class="ui input">
+            <input type="date" id="dataTermino">
+          </div>
+        </div>
+
+        <div class="ui label">
+          Faz horário de almoço?⠀⠀
+          <div class="ui input">
+            <select id="select-almoco" name="almoco" class="select2" style="border-color: red;" required>
+              <option value="S">Sim</option>
+              <option value="N">Não</option>
+            </select>
+          </div>
+        </div>
+
+        <br><br>
+
+
+        <div class="ui label">
+          Funcão⠀⠀
+          <div class="ui input">
+            <select id="select-funcao" name="funcao-funcionario" class="select2" style="border-color: red;" required>
+            </select>
+          </div>
+        </div>
+
+        <br><br>
+
+        <div class="ui large label">
+          Dias de Trabalho - Semanal</div>
+        <br><br>
+        <div class="ui slider checkbox">
+          <input type="checkbox" name="SEG" id="SEG">
+          <label for="SEG">Segunda</label>
+        </div>
+
+        <div class="ui slider checkbox">
+          <input type="checkbox" name="TER" id="TER">
+          <label for="TER">Terça</label>
+        </div>
+
+        <div class="ui slider checkbox">
+          <input type="checkbox" name="QUA" id="QUA">
+          <label for="QUA">Quarta</label>
+        </div>
+
+        <div class="ui slider checkbox">
+          <input type="checkbox" name="QUI" id="QUI">
+          <label for="QUI">Quinta</label>
+        </div>
+
+        <div class="ui slider checkbox">
+          <input type="checkbox" name="SEX" id="SEX">
+          <label for="SEX">Sexta</label>
+        </div>
+
+        <br><br>
+
+        <div class="ui fluid label">
+          Descrição do horário de Trabalho⠀⠀⠀
+          <div class="ui fluid icon input">
+            <input type="text" id="descricaoHorario" style="border-color: red;">
+            <i class="icon keyboard outline"></i>
+          </div>
+        </div>
+
+        <br><br>
+        <button class="ui right labeled icon green button" id="addfuncional">
+          <i class="plus square outline icon"></i>
+          Adicionar / Alterar
+        </button>
+
+
+        <table id="funcionalTable" class="ui blue celled table">
+          <thead>
+            <tr>
+              <th>Matrícula</th>
+              <th>Data Início</th>
+              <th>Data Final</th>
+              <th>Almoço?</th>
+              <th>Função</th>
+              <th>Dias de Trabalho</th>
+              <th>Descrição do horário</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+          </tbody>
+        </table>
+
+      </div>
 
     </div>
   </div>
@@ -177,57 +169,56 @@ include("./footer_menu.php");
       carregardadosFuncoes();
       carregardadosSetores();
 
+      var table = $('#funcionalTable').DataTable({
+        language: {
+          url: "//cdn.datatables.net/plug-ins/1.13.5/i18n/pt-BR.json",
+        },
+        "bFilter": false
+      });
+
+      $('#addfuncional').on('click', function() {
+        var matricula = $('#matricula').val();
+        var dataInicio = $('#dataInicio').val();
+        var dataTermino = $('#dataTermino').val();
+        var almoco = $('#select-almoco').val();
+        var funcao = $('#select-funcao').val();
+        var diasTrabalho = [];
+
+        if ($('#SEG').is(':checked')) diasTrabalho.push('Segunda');
+        if ($('#TER').is(':checked')) diasTrabalho.push('Terça');
+        if ($('#QUA').is(':checked')) diasTrabalho.push('Quarta');
+        if ($('#QUI').is(':checked')) diasTrabalho.push('Quinta');
+        if ($('#SEX').is(':checked')) diasTrabalho.push('Sexta');
+
+        var descricaoHorario = $('#descricaoHorario').val();
+
+        // Adicionar a nova linha à tabela
+        table.row.add([
+          matricula,
+          dataInicio,
+          dataTermino,
+          almoco,
+          funcao,
+          diasTrabalho.join(', '), // Transforma o array em uma string separada por vírgulas
+          descricaoHorario,
+          '<button class="ui icon red button">Remover</button>' // Botão para remover a linha
+        ]).draw(false); // Redesenha a tabela sem recarregar os dados
+
+        // Limpar campos após adicionar a linha
+        $('#matricula').val('');
+        $('#dataInicio').val('');
+        $('#dataTermino').val('');
+        $('#select-almoco').val('S');
+        $('#select-funcao').val('').val(null).trigger('change');
+        $('#SEG').prop('checked', false);
+        $('#TER').prop('checked', false);
+        $('#QUA').prop('checked', false);
+        $('#QUI').prop('checked', false);
+        $('#SEX').prop('checked', false);
+        $('#descricaoHorario').val('');
+      });
+
     });
-
-    const button = document.querySelector(".ui.right.labeled.icon.green.button");
-    button.addEventListener("click", collectData);
-
-
-    function collectData() {
-      // Create an empty object to store the data
-      const data = {};
-
-      // Get the values of all the input fields
-      const matricula = document.getElementById("matricula").value;
-      const dataInicio = document.getElementById("dataInicio").value;
-      const dataTermino = document.getElementById("dataTermino").value;
-      const almoco = document.getElementById("select-almoco").value;
-      const funcao = document.getElementById("select-funcao").value;
-      const diasTrabalho = [];
-      const descricaoHorario = document.getElementById("descricaoHorario").value;
-
-      // Get the values of all the checkboxes
-      const checkboxElements = document.querySelectorAll("input[type='checkbox']");
-      for (const checkboxElement of checkboxElements) {
-        if (checkboxElement.checked) {
-          diasTrabalho.push(checkboxElement.name);
-        }
-      }
-
-      // Add the data to the object
-      data.matricula = matricula;
-      data.dataInicio = dataInicio;
-      data.dataTermino = dataTermino;
-      data.almoco = almoco;
-      data.funcao = funcao;
-      data.diasTrabalho = diasTrabalho;
-      data.descricaoHorario = descricaoHorario;
-
-      // Present the data in the table
-      const table = document.querySelector("table");
-      table.querySelector("tbody").innerHTML = "";
-      for (const key in data) {
-        const tr = document.createElement("td");
-        const td = document.createElement("tr");
-        td.textContent = data[key];
-        tr.appendChild(td);
-        table.querySelector("tbody").appendChild(tr);
-      }
-
-      // Save the data in a variable
-      const variableName = "data";
-      window[variableName] = data;
-    }
 
 
     async function carregardadosFuncoes(FuncaoSalvoNoBanco = null) {
